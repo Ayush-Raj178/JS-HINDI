7c3ce967a637edf31c4a461c8506d016f0fb757f
Ayush-Raj178 <ayushraj12121212@gmail.com>
2024-09-26 20:01:27 +0000

    const var done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "ayush@gmail.com"
+var accountPassword = "09876"
+accountCity = "Bihar"
+let accountState;
+// accountId = 2
+accountEmail = "dhd@ut.com"
+accountPassword = "212121"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
