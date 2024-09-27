35ed58776617c4c82b0d18eda6712ab0203f233c
Ayush-Raj178 <ayushraj12121212@gmail.com>
2024-09-27 05:18:02 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all js code as new js
+
+//alert("hello!") // we r using nodejs not browser
+
+console.log(5+5); console.log("ayush") //code readability must imp**
+
+let name ="ayush"
+let age = 19
+let isloggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string=""
+//boolean => true/false
+//null =>  standalone value
+//undefined => not asign value
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
+
+
+
+
+
+
