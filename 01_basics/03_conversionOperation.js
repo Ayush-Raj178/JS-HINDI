236fcdedbea8463efbc131adf1a4b50935f51ba7
Ayush-Raj178 <ayushraj12121212@gmail.com>
2024-09-27 05:36:28 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "ayush"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abs" => NaN
+//true/false => 1/0
+
+let isloggedIn = "Ayush"
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+
+//true/false => 1/0
+//"" => false
+//"Ayush" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
