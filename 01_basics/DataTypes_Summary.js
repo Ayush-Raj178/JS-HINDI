ead9f62c498bdc2b4733dcf0a1ca269c384be17e
Ayush-Raj178 <ayushraj12121212@gmail.com>
2024-09-27 08:03:04 +0000

    dataTypes_Sumarry done

---
+++ 01_basics/DataTypes_Summary.js
@@ -0,0 +1,37 @@
+// # Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLogged = false
+const outsidetemp = null
+let userMail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+// const bigNumber = 374648448384n
+
+
+// # refrence type(Non primitive)
+
+// Array, Object, Functions
+
+const heros =["shaktiman","naagraj","doga"]
+let myObj = {
+    name: "ayush",
+    age: 222,
+}
+
+
+const myFunction = function(){
+    console.log("Hello World!");
+    
+}
+
+console.log(typeof anotherId);
+
