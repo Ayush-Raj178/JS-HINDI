f43877188fb42a41cce02dd83ab96dba75ffbe6c
Ayush-Raj178 <ayushraj12121212@gmail.com>
2024-09-28 06:48:19 +0000

    math_and_num Done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,45 @@
+const score = 400
+// console.log(score);
+// console.log(typeof score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(typeof balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 125.8966
+// console.log(otherNumber.toPrecision(4));
+
+
+const hunderds = 1000000
+// console.log(hunderds.toLocaleString('en-In'));
+
+//*********************************MATHS*******************************
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.min(4,3,6,7,8));
+// console.log(Math.max(4,3,6,7,8);
+
+
+console.log(Math.random()); // Math.random value (0 to 1)
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+// #FORMULA (MAX - MIN + 1)....NEED MORE THAN MIN SO IN (MAX - MIN + 1) WE ADD MIN ((MAX - MIN + 1) + MIN) 
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
