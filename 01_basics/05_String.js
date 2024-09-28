49a465881e0a4bb9bb7332fd1cb19783983c41d7
Ayush-Raj178 <ayushraj12121212@gmail.com>
2024-09-28 06:08:52 +0000

    String done

---
+++ 01_basics/05_String.js
@@ -0,0 +1,45 @@
+// const name = "Ayush Raj"
+// const repoCount = 50
+
+// console.log(name + repoCount +  ' value'); // # old and look messy
+
+// console.log(`Hello my name// is ${name} and my repoCount is ${repoCount}`); // # now in trending
+
+const gamename = new String('Ayush-Bgmi-noob')
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+console.log(gamename.length);
+
+
+// console.log(gamename.toUpperCase());
+
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('h'));
+
+// const myString = gamename.substring(0,6)
+// console.log(myString);
+
+// const newString = gamename.slice(-9,4)
+// console.log(newString);
+
+
+const newString1 = "  ayush  "
+console.log(newString1.trim());
+
+
+const url = "https://ayush.com/ayush%20raj"
+console.log(url.replace('%20','_'));
+
+console.log(url.includes('dumb'));
+
+console.log(gamename.split('-'));
+
+
+
+
+
+
+
+
+
